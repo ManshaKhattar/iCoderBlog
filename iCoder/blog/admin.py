52d22ad524854dc73b3fb232e37ac3d4ab4fd6de
ManshaKhattar <101034232+ManshaKhattar@users.noreply.github.com>
2022-09-25 12:55:18 +0530

    Add files via upload

---
+++ iCoder/blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from blog.models import BlogComment, Post
+# Register your models here.
+
+admin.site.register((BlogComment))
+
+@admin.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    class Media:
+        js= ('tinyInject.js',)
